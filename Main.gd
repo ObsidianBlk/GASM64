674aad7c51fe2dab953c3731fd4a9a35e9874e73
Bryan Miller <bmiller1008@gmail.com>
2021-09-11 17:26:52 -0400

    MemDevice now has a 'page' property. This new property is used by the Bus to determine where a MemDevice is connected, and will NOT connect a MemDevice that will overlap an existing device. Bus now also has a fill() method that will fill memory starting at one page and ending at another with a given value.

--- Main.gd
+++ Main.gd
@@ -4,6 +4,8 @@ const HZ = 1000000
 
 func _ready() -> void:
 	var bus = $Computer/Bus
+	bus.fill(0, 16, 0xEA)
+	bus.fill(255, 255, 0xEA)
 	bus.set_mem_addr(0xFFFC, 0x00)
 	bus.set_mem_addr(0xFFFD, 0x02)
 	
--- Scripts/Com/Bus.gd
+++ Scripts/Com/Bus.gd
@@ -13,48 +13,63 @@ var _mem_devices : Dictionary = {}
 # Override Methods
 # ---------------------------------------------------------------------------
 func _ready() -> void:
-	var page = 0
 	for child in get_children():
 		if child is MemDevice:
-			_mem_devices[[page, page + child.page_count]] = child
-			page += child.page_count
+			if _MemDevice(child.page << 8) < 0:
+				_mem_devices[child.page] = child
+			else:
+				print("WARNING: Unable to connect memory device. Starting page, ", child.page, ", collides with previously connected device.")
+
 
 # ---------------------------------------------------------------------------
 # Private Methods
 # ---------------------------------------------------------------------------
-
+func _MemDevice(addr : int) -> int:
+	if addr >= 0 and addr <= 0xFFFF:
+		var page = (addr & 0xFFFF) >> 8
+		for key in _mem_devices.keys():
+			var mdev = _mem_devices[key]
+			if page >= key and page < key + mdev.page_count:
+				return key
+	return -1
+	
 
 # ---------------------------------------------------------------------------
 # Public Methods
 # ---------------------------------------------------------------------------
 func write(addr : int, value : int) -> void:
-	if addr >= 0 and addr <= 0xFFFF:
-		var page = addr >> 8
-		for key in _mem_devices.keys():
-			if page >= key[0] and page < key[1]:
-				_mem_devices[key].write(addr - (key[0] << 8), value)
+	var page = _MemDevice(addr)
+	if page in _mem_devices:
+		_mem_devices[page].write(addr - (page << 8), value)
 
 func read(addr : int) -> int:
-	if addr >= 0 and addr <= 0xFFFF:
-		var page = addr >> 8
-		for key in _mem_devices.keys():
-			if page >= key[0] and page < key[1]:
-				return _mem_devices[key].read(addr - (key[0] << 8))
+	var page = _MemDevice(addr)
+	if page in _mem_devices:
+		return _mem_devices[page].read(addr - (page << 8))
 	return 0
 
 func set_mem_addr(addr : int, val : int) -> void:
-	if addr >= 0 and addr <= 0xFFFF:
-		var page = addr >> 8
-		for key in _mem_devices.keys():
-			#print("Target Page: ", page, " | Low Page: ", key[0], " | High Page: ", key[1])
-			if page >= key[0] and page < key[1]:
-				_mem_devices[key].set_mem_addr(addr - (key[0] << 8), val)
+	var page = _MemDevice(addr)
+	if page in _mem_devices:
+		_mem_devices[page].set_mem_addr(addr - (page << 8), val)
+
+func fill(from_page : int, to_page : int, value : int) -> void:
+	from_page = max(0, min(255, from_page))
+	to_page = max(from_page, min(255, to_page))
+	var page = from_page
+	while page <= to_page:
+		var dpage = _MemDevice(page << 8)
+		if dpage in _mem_devices:
+			for i in range(256):
+				_mem_devices[dpage].set_mem_addr((page << 8) | i, value)
+		page += 1
+
 
 func page_dump(page : int) -> PoolByteArray:
 	if page >= 0 and page <= 0xFF:
 		for key in _mem_devices.keys():
-			if page >= key[0] and page < key[1]:
-				return _mem_devices[key].page_dump(page - key[0])
+			if page >= key and page < key + _mem_devices[key].page_count:
+				return _mem_devices[key].page_dump(page - key)
 	return PoolByteArray([])
 
 func mem_dumb(offset : int, pages : int) -> PoolByteArray:
